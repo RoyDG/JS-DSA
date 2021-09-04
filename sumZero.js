c7031e7cfabb6cbe5b88b71d0a4bbdcd3f6ddccb
unknown <rdg01834@gmail.com>
2021-09-04 23:56:51 +0600

    Traversing

---
+++ sumZero.js
@@ -0,0 +1,15 @@
+//checking sum zero - problem 1
+//[-5,-4,-3,-2,0,2,4,6,8] >> input
+//[-4,4] >> output
+
+function getSumPairZero (array){
+    for (let number of array){
+        console.log(number)
+    }
+}
+
+getSumPairZero([-5,-4,-3,-2,0,2,4,6,8]);
+
+//Traversing: Traversing a Data Structure means to visit the element stored in it.
+// in here i'm checking the loop is running through every element of array.
+// for output check on terminal-- node sumZero
